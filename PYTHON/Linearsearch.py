3807bd15754afa30d2627c39fcc57b2905cd9728
majidkarigar <majidkarigar123@gmail.com>
2026-07-03 09:07:08 +0530

    Update Linearsearch.py

--- PYTHON/Linearsearch.py
+++ PYTHON/Linearsearch.py
@@ -1,1 +1,17 @@
+# Linear Search - Spam Detector
 
+B_list = [10, 20, 30, 40, 50]
+
+sender = int(input("Enter sender ID: "))
+
+found = 0
+
+for i in B_list:
+    if i == sender:
+        found = 1
+        break
+
+if found == 1:
+    print("Spam sender found")
+else:
+    print("No spam")
