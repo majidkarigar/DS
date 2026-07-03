e9dd4c9f4a9829197f6c2964405d7673e4309c43
majidkarigar <majidkarigar123@gmail.com>
2026-07-03 09:11:34 +0530

    Update BinarySearch.py

--- PYTHON/BinarySearch.py
+++ PYTHON/BinarySearch.py
@@ -1,1 +1,17 @@
+# Binary Search
 
+price = [15000, 25000, 35000, 45000, 55000]
+ta = 35000
+
+low = 0
+high = len(price) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if price[mid] < ta:
+        low = mid + 1
+    else:
+        high = mid - 1
+
+print("First price =", price[low])
